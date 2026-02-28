826601e0c44671dc26f50b6aab96159acf196ca3
rahulrameshm0 <iamrahulm23@gmail.com>
2026-02-28 20:34:41 +0530

    setting up pydantic data

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from fastapi.templating import Jinja2Templates
 from fastapi.exceptions import RequestValidationError
 from fastapi.responses import JSONResponse
 from starlette.exceptions import HTTPException as starletteHTTPException
-
+from schemas import PostCreate, PostResponse
 
 app = FastAPI()
 
@@ -48,17 +48,33 @@ def post_page(request: Request, post_id: int):
             )
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="post not found")
 
-@app.get('/api/posts')
+@app.get('/api/posts', response_model=list[PostResponse])
 def get_post():
     return posts  
 
-@app.get("/api/posts/{post_id}")
+@app.post(
+        "/api/posts",
+        response_model=PostResponse,
+        status_code=status.HTTP_201_CREATED,
+)
+def create_post(post:PostCreate):
+    new_id = max(p["id"] for p in posts) + 1 if posts else 1
+    new_post = {
+        "id": new_id,
+        "author": post.author,
+        "title": post.title,
+        "content": post.content,
+        "date_posted": "April 23rd, 2025",
+    }
+    posts.append(new_post)
+    return new_post
+
+@app.get("/api/posts/{post_id}", response_model=PostResponse)
 def get_post(post_id: int):
     for post in posts:
         if post.get("id") == post_id:
             return post
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="post not found")
-
 
 @app.exception_handler(starletteHTTPException)
 def general_http_exception_handler(request:Request, exception: starletteHTTPException):
