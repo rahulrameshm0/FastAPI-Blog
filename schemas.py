b5f812dd584affca5e4d34e33a3776a1a97d8811
rahulrameshm0 <iamrahulm23@gmail.com>
2026-03-02 13:18:28 +0530

    setting up schemas

--- schemas.py
+++ schemas.py
@@ -1,6 +1,21 @@
 from datetime import datetime
 from pydantic import BaseModel, ConfigDict, Field, EmailStr
 
+
+class UserBase(BaseModel):
+    username: str = Field(min_length=1, max_length=50)
+    email: EmailStr = Field(max_digits=120)
+
+class UserCreate(UserBase):
+    pass
+
+class UserResponse(UserBase):
+    model_config  = ConfigDict(from_attributes=True)
+    id: int
+    image_filed: str | None
+    image_path: str
+
+
 class PostBase(BaseModel):
     title: str = Field(min_length=1, max_length=100)
     content: str = Field(min_length=1)
